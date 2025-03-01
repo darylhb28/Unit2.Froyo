f87ec274e08e17e8ec0433ad7e5b1f4569ea78ba
Daryl Hornick-Becker <darylhornickbecker@gmail.com>
2025-03-01 13:17:36 -0500

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const userInputString = prompt(
-    "Please enter  a list of comma-separated froyo flavors",
+    "Please enter a list of comma-separated froyo flavors, for example:",
     "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
@@ -8,26 +8,21 @@ const froyoArray = userInputString.split(",");
 
 console.log(froyoArray)
 
-const froyoOrder = {};
-
-for (let i = 0; i < froyoArray.length; i++) {
-    let flavor = froyoArray[i]
-    if (froyoOrder[flavor]) {
-        froyoOrder[flavor]++; 
-    } else { froyoOrder[flavor] = 1; 
-    }
-}
-
-console.log(froyoOrder)
-
-function condenseOrder(obj){
-    for (let i = 0; i < Array.length; i++) {
-        let value = Array[i]
-        if (obj[value]) {
-            obj[value]++; 
-        } else { obj[value] = 1; 
+function countFlavors(froyoArray){
+    let froyoOrder = {};
+    for (let i = 0; i < froyoArray.length; i++) {
+        let flavor = froyoArray[i]
+        if (froyoOrder[flavor]) {
+            froyoOrder[flavor]++; 
+        } else { froyoOrder[flavor] = 1; 
         }
-    }
 }
+return froyoOrder
+}
+
+const froyoOrder = countFlavors(froyoArray)
+
+console.table(froyoOrder)
+
 
 
